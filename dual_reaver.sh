7ff305b1d2d23214dbf0f351fa810aa848bad9e2
duchenpaul <duchenpaul@gmail.com>
2014-09-24 23:22:05 +0800

    Update dual_reaver.sh

--- dual_reaver.sh
+++ dual_reaver.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+echo ==start cracking==
+
 while true
 do 
-command1="sh /root/Music/5D3A.sh"  #reaver command 1
-command2="sh /root/Music/B118.sh"  #reaver command 2
+command1="sh /root/Music/task_1.sh"  #reaver command 1
+command2="sh /root/Music/task_2.sh"  #reaver command 2
 thisdate=`date "+%Y-%m-%d %H:%M:%S" `
 # $command1
- $command1 >>b.txt & 
+echo =====1===========1============1================1===============1==============
+ $command1 & 
  sleep 60
   thispid=`ps -ef|grep reaver |grep -v grep|awk '{ print $2}'`
   echo a $thisdate >>a.txt
@@ -13,7 +16,8 @@ thisdate=`date "+%Y-%m-%d %H:%M:%S" `
   echo a `date "+%Y-%m-%d %H:%M:%S" ` $thispid
   kill -2 $thispid
 #$command2
- $command2 >>b.txt &
+echo =========2=============2==============2=============2===========2=========2
+ $command2 &
  sleep 60
   thispid=`ps -ef|grep reaver |grep -v grep|awk '{ print $2}'`
  echo b $thisdate >>a.txt
@@ -21,3 +25,4 @@ thisdate=`date "+%Y-%m-%d %H:%M:%S" `
  echo b `date "+%Y-%m-%d %H:%M:%S" ` $thispid
  kill -2 $thispid
 done
+
